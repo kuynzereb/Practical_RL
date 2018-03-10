7f9cae93cfd62c73de61c46fb38d05598f514542
Vadim Bereznyuk <kuynzereb@gmail.com>
2018-03-10 18:42:27 +0300

    week3 upd

--- week3_model_free/homework/expected_value_sarsa.py
+++ week3_model_free/homework/expected_value_sarsa.py
@@ -74,7 +74,9 @@ class EVSarsaAgent():
     #You'll need this to estimate action probabilities
     epsilon = self.epsilon
     
-    value = <Your Code Here>
+    value = epsilon \
+            * np.mean([self.getQValue(state, a) for a in possibleActions]) \
+            + (1 - epsilon) * self.getQValue(state, self.getPolicy(state))
     return value
     
   def getPolicy(self, state):
@@ -120,6 +122,137 @@ class EVSarsaAgent():
     	return random.choice(possibleActions)
     else:
     	action = self.getPolicy(state)
+    return action
+
+  def update(self, state, action, nextState, reward):
+    """
+      You should do your Q-Value update here
+
+      NOTE: You should never call this function,
+      it will be called on your behalf
+
+
+    """
+    #agent parameters
+    gamma = self.discount
+    learning_rate = self.alpha
+    
+    reference_qvalue = reward + gamma * self.getValue(nextState)
+    updated_qvalue = (1-learning_rate) * self.getQValue(state,action) + learning_rate * reference_qvalue
+    self.setQValue(state,action,updated_qvalue)
+
+
+#---------------------#end of your code#---------------------#
+
+
+class SoftMaxEVSarsaAgent():
+  """
+    Expected Value SARSA Agent.
+    
+    The two main methods are 
+    - self.getAction(state) - returns agent's action in that state
+    - self.update(state,action,nextState,reward) - returns agent's next action
+
+    Instance variables you have access to
+      - self.epsilon (exploration prob)
+      - self.alpha (learning rate)
+      - self.discount (discount rate aka gamma)
+
+  """
+  def __init__(self,alpha,epsilon,discount,theta,getLegalActions):
+    "We initialize agent and Q-values here."
+    self.getLegalActions= getLegalActions
+    self._qValues = defaultdict(lambda:defaultdict(lambda:0))
+    self.alpha = alpha
+    self.epsilon = epsilon
+    self.discount = discount
+    self.theta = theta
+    
+  def getQValue(self, state, action):
+    """
+      Returns Q(state,action)
+    """
+    return self._qValues[state][action]
+
+  def setQValue(self,state,action,value):
+    """
+      Sets the Qvalue for [state,action] to the given value
+    """
+    self._qValues[state][action] = value
+
+#---------------------#start of your code#---------------------#
+
+  def getValue(self, state):
+    """
+      Returns V(s) according to expected value SARSA algorithm
+      This should be equal to expected action q-value over action probabilities defined
+      by epsilon-greedy policy with current epsilon.
+    """
+    
+    possibleActions = self.getLegalActions(state)
+    #If there are no legal actions, return 0.0
+    if len(possibleActions) == 0:
+      return 0.0
+
+    #You'll need this to estimate action probabilities
+    epsilon = self.epsilon
+    theta = self.theta
+    values = np.array([self.getQValue(state, a) for a in possibleActions])
+    exps = np.exp(values / theta - np.max(values / theta))
+    probs = exps / np.sum(exps)
+    
+    value = epsilon * np.mean(values) \
+            + (1 - epsilon) * np.sum(probs * values)
+    return value
+    
+  def getPolicy(self, state):
+    """
+      Compute the best action to take in a state. 
+      
+    """
+    possibleActions = self.getLegalActions(state)
+
+    #If there are no legal actions, return None
+    if len(possibleActions) == 0:
+      return None
+    
+    epsilon = self.epsilon
+    theta = self.theta
+    values = np.array([self.getQValue(state, a) for a in possibleActions])
+    exps = np.exp(values / theta - np.max(values / theta))
+    probs = exps / np.sum(exps)
+
+    action = np.random.choice(possibleActions, p=probs)
+
+    return action
+
+  def getAction(self, state):
+    """
+      Compute the action to take in the current state, including exploration.  
+      
+      With probability self.epsilon, we should take a random action.
+      otherwise - the best policy action (self.getPolicy).
+
+      HINT: You might want to use util.flipCoin(prob)
+      HINT: To pick randomly from a list, use random.choice(list)
+
+    """
+    
+    # Pick Action
+    possibleActions = self.getLegalActions(state)
+    action = None
+    
+    #If there are no legal actions, return None
+    if len(possibleActions) == 0:
+      return None
+
+    #agent parameters:
+    epsilon = self.epsilon
+
+    if np.random.random()<=epsilon:
+      return random.choice(possibleActions)
+    else:
+      action = self.getPolicy(state)
     return action
 
   def update(self, state, action, nextState, reward):
--- week3_model_free/homework/qlearning.py
+++ week3_model_free/homework/qlearning.py
@@ -1,140 +1,152 @@
 """
 Q-learning
 This file contains the same q-learning agent you implemented in the previous assignment.
-The only difference is that it doesn't need any other files with it, so you can use it as a standalone moule.
+The only difference is that it doesn't need any other files with it, so you can use it as a standalone module.
 
 Here's an example:
 >>>from qlearning import QLearningAgent
 
 >>>agent = QLearningAgent(alpha=0.5,epsilon=0.25,discount=0.99,
-                       getLegalActions = lambda s: actions_from_that_state)
+                                             getLegalActions = lambda s: actions_from_that_state)
 >>>action = agent.getAction(state)
 >>>agent.update(state,action, next_state,reward)
 >>>agent.epsilon *= 0.99
 """
 
-import random,math
+import random, math
 
 import numpy as np
 from collections import defaultdict
 
+def flipCoin(p):
+  r = random.random()
+  return r < p 
+
 class QLearningAgent():
-  """
-    Q-Learning Agent
-
-    The two main methods are 
-    - self.getAction(state) - returns agent's action in that state
-    - self.update(state,action,nextState,reward) - returns agent's next action
-
-    Functions you should use
-      - self.getLegalActions(state)
-        which returns legal actions for a state
-      - self.getQValue(state,action)
-        which returns Q(state,action)
-      - self.setQValue(state,action,value)
-        which sets Q(state,action) := value
-    
-    !!!Important!!!
-    NOTE: please avoid using self._qValues directly to make code cleaner
-  """
-  def __init__(self,alpha,epsilon,discount,getLegalActions):
-    "We initialize agent and Q-values here."
-    self.getLegalActions= getLegalActions
-    self._qValues = defaultdict(lambda:defaultdict(lambda:0))
-    self.alpha = alpha
-    self.epsilon = epsilon
-    self.discount = discount
-
-  def getQValue(self, state, action):
     """
-      Returns Q(state,action)
-    """
-    return self._qValues[state][action]
+        Q-Learning Agent
 
-  def setQValue(self,state,action,value):
+        The two main methods are 
+        - self.getAction(state) - returns agent's action in that state
+        - self.update(state,action,nextState,reward) - returns agent's next action
+
+        Functions you should use
+            - self.getLegalActions(state)
+                which returns legal actions for a state
+            - self.getQValue(state,action)
+                which returns Q(state,action)
+            - self.setQValue(state,action,value)
+                which sets Q(state,action) := value
+        
+        !!!Important!!!
+        NOTE: please avoid using self._qValues directly to make code cleaner
     """
-      Sets the Qvalue for [state,action] to the given value
-    """
-    self._qValues[state][action] = value
+    def __init__(self,alpha,epsilon,discount,getLegalActions):
+        "We initialize agent and Q-values here."
+        self.getLegalActions= getLegalActions
+        self._qValues = defaultdict(lambda:defaultdict(lambda:0))
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.discount = discount
+
+    def getQValue(self, state, action):
+        """
+            Returns Q(state,action)
+        """
+        return self._qValues[state][action]
+
+    def setQValue(self,state,action,value):
+        """
+            Sets the Qvalue for [state,action] to the given value
+        """
+        self._qValues[state][action] = value
 
 #---------------------#start of your code#---------------------#
 
-  def getValue(self, state):
-    """
-      Returns max_action Q(state,action)
-      where the max is over legal actions.
-    """
-    
-    possibleActions = self.getLegalActions(state)
-    #If there are no legal actions, return 0.0
-    if len(possibleActions) == 0:
-    	return 0.0
+    def getValue(self, state):
+        """
+            Returns max_action Q(state,action)
+            where the max is over legal actions.
+        """
+        
+        possibleActions = self.getLegalActions(state)
+        #If there are no legal actions, return 0.0
+        if len(possibleActions) == 0:
+            return 0.0
 
-    "*** YOUR CODE HERE ***"
-    return <compute state value>
-    
-  def getPolicy(self, state):
-    """
-      Compute the best action to take in a state. 
-      
-    """
-    possibleActions = self.getLegalActions(state)
+        max_value = self.getQValue(state, possibleActions[0])
+        for action in possibleActions:
+            value = self.getQValue(state, action)
+            max_value = value if value > max_value else max_value
 
-    #If there are no legal actions, return None
-    if len(possibleActions) == 0:
-    	return None
-    
-    best_action = None
+        return max_value
+        
+    def getPolicy(self, state):
+        """
+            Compute the best action to take in a state. 
+            
+        """
+        possibleActions = self.getLegalActions(state)
 
-    "*** YOUR CODE HERE ***"
-    best_action = <your code>
-    return best_action
+        #If there are no legal actions, return None
+        if len(possibleActions) == 0:
+            return None
+        
+        max_value = self.getValue(state)
+        for action in possibleActions:
+            if self.getQValue(state, action) == max_value:
+                return action
 
-  def getAction(self, state):
-    """
-      Compute the action to take in the current state, including exploration.  
-      
-      With probability self.epsilon, we should take a random action.
-      otherwise - the best policy action (self.getPolicy).
+        return None
 
-      HINT: You might want to use util.flipCoin(prob)
-      HINT: To pick randomly from a list, use random.choice(list)
+    def getAction(self, state):
+        """
+            Compute the action to take in the current state, including exploration.  
+            
+            With probability self.epsilon, we should take a random action.
+            otherwise - the best policy action (self.getPolicy).
 
-    """
-    
-    # Pick Action
-    possibleActions = self.getLegalActions(state)
-    action = None
-    
-    #If there are no legal actions, return None
-    if len(possibleActions) == 0:
-    	return None
+            HINT: You might want to use util.flipCoin(prob)
+            HINT: To pick randomly from a list, use random.choice(list)
 
-    #agent parameters:
-    epsilon = self.epsilon
+        """
+        
+        # Pick Action
+        possibleActions = self.getLegalActions(state)
+        action = None
+        
+        #If there are no legal actions, return None
+        if len(possibleActions) == 0:
+            return None
 
-    "*** YOUR CODE HERE ***"
-    
-    return <put agent's action here>
+        #agent parameters:
+        epsilon = self.epsilon
 
-  def update(self, state, action, nextState, reward):
-    """
-      You should do your Q-Value update here
+        if flipCoin(epsilon):
+            action = random.choice(possibleActions)
+        else:
+            action = self.getPolicy(state)   
 
-      NOTE: You should never call this function,
-      it will be called on your behalf
+        return action
+
+    def update(self, state, action, nextState, reward):
+        """
+            You should do your Q-Value update here
+
+            NOTE: You should never call this function,
+            it will be called on your behalf
 
 
-    """
-    #agent parameters
-    gamma = self.discount
-    learning_rate = self.alpha
-    
-    "*** YOUR CODE HERE ***"    
-    reference_qvalue = <the "correct state value", uses reward and the value of next state>
-    
-    updated_qvalue = (1-learning_rate) * self.getQValue(state,action) + learning_rate * reference_qvalue
-    self.setQValue(state,action,updated_qvalue)
+        """
+        #agent parameters
+        gamma = self.discount
+        lr = self.alpha
+
+        reference_qvalue = reward + gamma * self.getValue(nextState)
+        updated_qvalue = (1 - lr) * self.getQValue(state, action) + \
+                         lr * reference_qvalue
+
+        self.setQValue(state, action, updated_qvalue)
 
 
 #---------------------#end of your code#---------------------#
